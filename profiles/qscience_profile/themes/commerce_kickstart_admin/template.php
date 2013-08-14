7adec31c63a2dd0b9ac648f4cb5f4dbda0c1bbce
drozas <david.rozas@gmail.com>
2013-08-14 14:14:34 +0100

    New release including patterns_client

---
+++ profiles/qscience_profile/themes/commerce_kickstart_admin/template.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Override or insert variables into the maintenance page template.
+ */
+function commerce_kickstart_admin_preprocess_maintenance_page(&$vars) {
+  // While markup for normal pages is split into page.tpl.php and html.tpl.php,
+  // the markup for the maintenance page is all in the single
+  // maintenance-page.tpl.php template. So, to have what's done in
+  // shiny_preprocess_html() also happen on the maintenance page, it has to be
+  // called here.
+  shiny_preprocess_html($vars);
+/*  if (variable_get('install_task') != 'done') {
+    $footer_markup =  '<div class="message">' . t('Built and maintained by developers working on') . '</div>';
+    $footer_markup .=  '<div class="logo">' . t('<a href="@url">QLectives</a>', array('@url' => 'http://www.qlectives.eu')) . '</div>';
+    $vars['footer'] = array(
+      '#prefix' => '<div id="credit" class="clearfix">',
+      '#markup' => $footer_markup,
+      '#suffix' => '</div>',
+    );
+  }
+  */
+}
